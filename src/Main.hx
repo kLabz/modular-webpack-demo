d943d0690a992cdfa49fc75a5574b9f2282212a1
k <k@klabz.org>
2018-01-07 23:16:43 +0100

    bug with level 1 bundle

--- src/Main.hx
+++ src/Main.hx
@@ -5,6 +5,11 @@ class Main {
 		Webpack.load(app2.App2)
 			.then(function(mod) {
 				app2.App2.load();
+
+				Webpack.load(app1.App1)
+					.then(function(mod) {
+						trace('test');
+					});
 			});
 	}
 }
--- src/app1/App1.hx
+++ src/app1/App1.hx
@@ -1,5 +1,5 @@
 package app1;
 
 @:expose('default')
-class App1 extends ReactComponent {}
+class App1 {}
 
